ed4f616f30550aa0bb3044c8dbaab966bb739bb6
Francisco <franciscolopezjr012@gmail.com>
2021-09-29 18:14:36 -0500

    baisc JS code to play one round against the computer

---
+++ script.js
@@ -0,0 +1,33 @@
+const options = ["rock", 'paper', 'scissors'];
+
+function computerPlay(_computerSelection) {
+    return options[Math.floor(Math.random() * 3)]
+}
+
+function playerPlay(_playerSelection) {
+    return prompt('choose an option! ', '')
+}
+
+
+const computerSelection = computerPlay();
+const playerSelection = playerPlay();
+alert('Computer chose ' + computerPlay())
+
+if (playerSelection === computerSelection) {
+    alert ('Tie Game!')
+}else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    alert ('You lose! Paper beats Rock!');
+} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    alert ('You Win! Rock beats Scissors')
+} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    alert ('You Lose! Scissors beats Paper!')
+} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    alert ('You Win! Paper beats Scissors')
+} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    alert ('You lose! Rock beats Scissors')
+} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    alert ('You Win! Scissors beats Paper')
+}
+     
+
+
